abd44389024dd540deb438153ab2f6cdd72e56ea
Bertrand Bonnefoy-Claudet <bertrand@cryptosense.com>
2018-10-19 03:12:03 -0700

    Add data collector

---
+++ CoreSample.CsDataCollector/CsDataCollector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
+
+[DataCollectorFriendlyName("CsDataCollector")]
+[DataCollectorTypeUri("my://sample/datacollector")]
+public class CsDataCollector : DataCollector, ITestExecutionEnvironmentSpecifier
+{
+    public override void Initialize(
+        System.Xml.XmlElement configurationElement,
+        DataCollectionEvents events,
+        DataCollectionSink dataSink,
+        DataCollectionLogger logger,
+        DataCollectionEnvironmentContext environmentContext)
+    {
+    }
+
+    public IEnumerable<KeyValuePair<string, string>> GetTestExecutionEnvironmentVariables()
+    {
+        return new List<KeyValuePair<string, string>> { new KeyValuePair<string, string>("key", "value") };
+    }
+}
