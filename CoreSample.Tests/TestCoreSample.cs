362839486636e1ae0d69a945967660885817f66d
Bertrand Bonnefoy-Claudet <bertrand@cryptosense.com>
2018-12-11 14:01:38 +0100

    Add SHA1 test case

--- CoreSample.Tests/TestCoreSample.cs
+++ CoreSample.Tests/TestCoreSample.cs
@@ -21,5 +21,22 @@ namespace CoreSample.Tests
                 }
             );
         }
+
+        [TestMethod]
+        public void TestSha1()
+        {
+            byte[] message = new byte[] { 0, 1, 2 };
+
+            byte[] result = CoreSample.Program.Sha1(message);
+
+            CollectionAssert.AreEqual(
+                result,
+                new byte[] {
+                    0x0c, 0x7a, 0x62, 0x3f, 0xd2, 0xbb, 0xc0, 0x5b,
+                    0x06, 0x42, 0x3b, 0xe3, 0x59, 0xe4, 0x02, 0x1d,
+                    0x36, 0xe7, 0x21, 0xad
+                }
+            );
+        }
     }
 }
--- CoreSample/Program.cs
+++ CoreSample/Program.cs
@@ -13,11 +13,25 @@ namespace CoreSample
             }
         }
 
+        public static byte[] Sha1(byte[] message)
+        {
+            using (SHA1 sha1 = SHA1.Create())
+            {
+                return sha1.ComputeHash(message);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("md5DirectArray");
+            Console.WriteLine("Md5");
             byte[] message = new byte[] { 0, 1, 2 };
             byte[] hash = Md5(message);
+            Console.WriteLine("  Message: " + string.Join(",", message));
+            Console.WriteLine("  Hash: " + string.Join(",", hash));
+
+            Console.WriteLine("Sha1");
+            message = new byte[] { 0, 1, 2 };
+            hash = Sha1(message);
             Console.WriteLine("  Message: " + string.Join(",", message));
             Console.WriteLine("  Hash: " + string.Join(",", hash));
         }
