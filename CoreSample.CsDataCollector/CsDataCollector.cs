0db73d4372d75a21a3c4e7d3bbaa50e1ef6bca8d
Bertrand Bonnefoy-Claudet <bertrand@cryptosense.com>
2018-12-13 10:30:42 +0100

    Prepare collector for shipping

--- CoreSample.CsDataCollector/CsDataCollector.cs
+++ CoreSample.CsDataCollector/CsDataCollector.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-
+using System.Xml;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
 
 [DataCollectorFriendlyName("CsDataCollector")]
 [DataCollectorTypeUri("my://sample/datacollector")]
 public class CsDataCollector : DataCollector, ITestExecutionEnvironmentSpecifier
 {
+    private string tracerPath;
+    private string outputDir;
+
     public override void Initialize(
         System.Xml.XmlElement configurationElement,
         DataCollectionEvents events,
@@ -15,10 +18,17 @@ public class CsDataCollector : DataCollector, ITestExecutionEnvironmentSpecifier
         DataCollectionLogger logger,
         DataCollectionEnvironmentContext environmentContext)
     {
+        tracerPath = configurationElement["TracerPath"].InnerText;
+        outputDir = configurationElement["OutputDir"].InnerText;
     }
 
     public IEnumerable<KeyValuePair<string, string>> GetTestExecutionEnvironmentVariables()
     {
-        return new List<KeyValuePair<string, string>> { new KeyValuePair<string, string>("key", "value") };
+        return new List<KeyValuePair<string, string>> {
+            new KeyValuePair<string, string>("CORECLR_ENABLE_PROFILING", "1"),
+            new KeyValuePair<string, string>("CORECLR_PROFILER", "{cf0d821e-299b-5307-a3d8-9ccb4916d2e5}"),
+            new KeyValuePair<string, string>("CORECLR_PROFILER_PATH", tracerPath),
+            new KeyValuePair<string, string>("CS_OUTPUT_DIR", outputDir),
+        };
     }
 }
--- CoreSample.Tests/TestCoreSample.cs
+++ CoreSample.Tests/TestCoreSample.cs
@@ -1,5 +1,4 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Linq;
 
 namespace CoreSample.Tests
 {
